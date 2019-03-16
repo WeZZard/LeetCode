7f671a516b3beb60c111e5a778241687eaae4958
WeZZard <me@wezzard.com>
2019-03-17 05:43:15 +0800

    Refines a solution for problem 65: Valid Number.

--- 65-Valid-Number/Sources/ValidNumber/ValidNumber.swift
+++ 65-Valid-Number/Sources/ValidNumber/ValidNumber.swift
@@ -1,3 +1,19 @@
+let zero = "0".utf8.first!
+let one = "1".utf8.first!
+let two = "2".utf8.first!
+let three = "3".utf8.first!
+let four = "4".utf8.first!
+let five = "5".utf8.first!
+let six = "6".utf8.first!
+let seven = "7".utf8.first!
+let eight = "8".utf8.first!
+let nine = "9".utf8.first!
+let plus = "+".utf8.first!
+let minus = "-".utf8.first!
+let dot = ".".utf8.first!
+let e = "e".utf8.first!
+let whitespace = " ".utf8.first!
+
 class Solution {
     func isNumber(_ s: String) -> Bool {
         var next: Int? = 0
@@ -31,178 +47,150 @@ class Solution {
     }
 
     func s0(_ character: UInt8) -> Int? {
-        switch character {
-            case "0".utf8.first!, 
-                "1".utf8.first!,
-                "2".utf8.first!,
-                "3".utf8.first!,
-                "4".utf8.first!,
-                "5".utf8.first!,
-                "6".utf8.first!,
-                "7".utf8.first!,
-                "8".utf8.first!,
-                "9".utf8.first!:
-                return 1
-            case "+".utf8.first!:   return 2
-            case "-".utf8.first!:   return 2
-            case ".".utf8.first!:   return 3
-            case "e".utf8.first!:   return nil
-            case " ".utf8.first!:   return 0
-            default:    return nil
+        struct Static {
+            static let table = [
+                zero: 1,
+                one: 1,
+                two: 1,
+                three: 1,
+                four: 1,
+                five: 1,
+                six: 1,
+                seven: 1,
+                eight: 1,
+                nine: 1,
+                plus: 2,
+                minus: 2,
+                dot: 3,
+                whitespace: 0,
+            ]
         }
+        return Static.table[character]
     }
 
     func s1(_ character: UInt8) -> Int? {
-        switch character {
-            case "0".utf8.first!, 
-                "1".utf8.first!,
-                "2".utf8.first!,
-                "3".utf8.first!,
-                "4".utf8.first!,
-                "5".utf8.first!,
-                "6".utf8.first!,
-                "7".utf8.first!,
-                "8".utf8.first!,
-                "9".utf8.first!:
-                return 1
-            case "+".utf8.first!:   return nil
-            case "-".utf8.first!:   return nil
-            case ".".utf8.first!:   return 4
-            case "e".utf8.first!:   return 5
-            case " ".utf8.first!:   return 6
-            default:    return nil
+        struct Static {
+            static let table = [
+                zero: 1,
+                one: 1,
+                two: 1,
+                three: 1,
+                four: 1,
+                five: 1,
+                six: 1,
+                seven: 1,
+                eight: 1,
+                nine: 1,
+                dot: 4,
+                e: 5,
+                whitespace: 6,
+            ]
         }
+        return Static.table[character]
     }
 
     func s2(_ character: UInt8) -> Int? {
-        switch character {
-            case "0".utf8.first!, 
-                "1".utf8.first!,
-                "2".utf8.first!,
-                "3".utf8.first!,
-                "4".utf8.first!,
-                "5".utf8.first!,
-                "6".utf8.first!,
-                "7".utf8.first!,
-                "8".utf8.first!,
-                "9".utf8.first!:
-                return 1
-            case "+".utf8.first!:   return nil
-            case "-".utf8.first!:   return nil
-            case ".".utf8.first!:   return 3
-            case "e".utf8.first!:   return nil
-            case " ".utf8.first!:   return nil
-            default:    return nil
+        struct Static {
+            static let table = [
+                zero: 1,
+                one: 1,
+                two: 1,
+                three: 1,
+                four: 1,
+                five: 1,
+                six: 1,
+                seven: 1,
+                eight: 1,
+                nine: 1,
+                dot: 3,
+            ]
         }
+        return Static.table[character]
     }
 
     func s3(_ character: UInt8) -> Int? {
-        switch character {
-            case "0".utf8.first!, 
-                "1".utf8.first!,
-                "2".utf8.first!,
-                "3".utf8.first!,
-                "4".utf8.first!,
-                "5".utf8.first!,
-                "6".utf8.first!,
-                "7".utf8.first!,
-                "8".utf8.first!,
-                "9".utf8.first!:
-                return 4
-            case "+".utf8.first!:   return nil
-            case "-".utf8.first!:   return nil
-            case ".".utf8.first!:   return nil
-            case "e".utf8.first!:   return nil
-            case " ".utf8.first!:   return nil
-            default:    return nil
+        struct Static {
+            static let table = [
+                zero: 4,
+                one: 4,
+                two: 4,
+                three: 4,
+                four: 4,
+                five: 4,
+                six: 4,
+                seven: 4,
+                eight: 4,
+                nine: 4,
+            ]
         }
+        return Static.table[character]
     }
 
     func s4(_ character: UInt8) -> Int? {
-        switch character {
-            case "0".utf8.first!, 
-                "1".utf8.first!,
-                "2".utf8.first!,
-                "3".utf8.first!,
-                "4".utf8.first!,
-                "5".utf8.first!,
-                "6".utf8.first!,
-                "7".utf8.first!,
-                "8".utf8.first!,
-                "9".utf8.first!:
-                return 4
-            case "+".utf8.first!:   return nil
-            case "-".utf8.first!:   return nil
-            case ".".utf8.first!:   return nil
-            case "e".utf8.first!:   return 5
-            case " ".utf8.first!:   return 6
-            default:    return nil
+        struct Static {
+            static let table = [
+                zero: 4,
+                one: 4,
+                two: 4,
+                three: 4,
+                four: 4,
+                five: 4,
+                six: 4,
+                seven: 4,
+                eight: 4,
+                nine: 4,
+                e: 5,
+                whitespace: 6,
+            ]
         }
+        return Static.table[character]
     }
 
     func s5(_ character: UInt8) -> Int? {
-        switch character {
-            case "0".utf8.first!, 
-                "1".utf8.first!,
-                "2".utf8.first!,
-                "3".utf8.first!,
-                "4".utf8.first!,
-                "5".utf8.first!,
-                "6".utf8.first!,
-                "7".utf8.first!,
-                "8".utf8.first!,
-                "9".utf8.first!:
-                return 8
-            case "+".utf8.first!:   return 3
-            case "-".utf8.first!:   return 3
-            case ".".utf8.first!:   return nil
-            case "e".utf8.first!:   return nil
-            case " ".utf8.first!:   return nil
-            default:    return nil
+        struct Static {
+            static let table = [
+                zero: 8,
+                one: 8,
+                two: 8,
+                three: 8,
+                four: 8,
+                five: 8,
+                six: 8,
+                seven: 8,
+                eight: 8,
+                nine: 8,
+                plus: 3,
+                minus: 3,
+            ]
         }
+        return Static.table[character]
     }
 
     func s6(_ character: UInt8) -> Int? {
-        switch character {
-            case "0".utf8.first!, 
-                "1".utf8.first!,
-                "2".utf8.first!,
-                "3".utf8.first!,
-                "4".utf8.first!,
-                "5".utf8.first!,
-                "6".utf8.first!,
-                "7".utf8.first!,
-                "8".utf8.first!,
-                "9".utf8.first!:
-                return nil
-            case "+".utf8.first!:   return nil
-            case "-".utf8.first!:   return nil
-            case ".".utf8.first!:   return nil
-            case "e".utf8.first!:   return nil
-            case " ".utf8.first!:   return 6
-            default:    return nil
+        struct Static {
+            static let table = [
+                whitespace: 6,
+            ]
         }
+        return Static.table[character]
     }
 
     func s8(_ character: UInt8) -> Int? {
-        switch character {
-            case "0".utf8.first!, 
-                "1".utf8.first!,
-                "2".utf8.first!,
-                "3".utf8.first!,
-                "4".utf8.first!,
-                "5".utf8.first!,
-                "6".utf8.first!,
-                "7".utf8.first!,
-                "8".utf8.first!,
-                "9".utf8.first!:
-                return 8
-            case "+".utf8.first!:   return nil
-            case "-".utf8.first!:   return nil
-            case ".".utf8.first!:   return nil
-            case "e".utf8.first!:   return nil
-            case " ".utf8.first!:   return 6
-            default:    return nil
+        struct Static {
+            static let table = [
+                zero: 8,
+                one: 8,
+                two: 8,
+                three: 8,
+                four: 8,
+                five: 8,
+                six: 8,
+                seven: 8,
+                eight: 8,
+                nine: 8,
+                whitespace: 6,
+            ]
         }
+        return Static.table[character]
     }
 }
